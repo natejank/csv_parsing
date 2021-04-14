228dd3196d4c94aae67f3d5ea6aec9cb73fd6e97
natejank <nate@jankowskis.net>
2021-04-14 01:22:08 -0400

    Refactoring to own me from 2 years ago
    Oh the places you'll go :^)

--- Parse.py
+++ Parse.py
@@ -1,28 +1,21 @@
-list_of_teams=[] #list of teams
-schedule_by_match={} #dict of schedule_by_match by match
-schedule_by_team={} #dict of schedule_by_match by team
-csv_file="Scouting_2019_Match_Schedule.csv"
+teams = {}
 
-file=open(csv_file,'r')
-for line in file.readlines(): 
-    line=line.replace('\n','')  
-    line=line.split(",") 
- 
-    if line[0].isnumeric():
-        lineKey=line[0]
-        line.pop(0) 
-        schedule_by_team.update({lineKey:line})
-        for i in range(0,len(line)-1):
-            if line[i] not in list_of_teams:
-                list_of_teams.append(line[i])
-file.close()
+for line in open('Scouting_2019_Match_Schedule.csv', 'r').readlines():
+    line = line.rstrip('/n') # line[:-1] doesn't work because the file doesn't end with a newline
+    line = line.split(',')
 
-for i in range (0,len(list_of_teams)-1): 
-    matches=[]
-    for j in range (0,len(schedule_by_team.keys())-1):
-        if list_of_teams[i] in schedule_by_team.get(list(schedule_by_team.keys())[j]):
-            matches.append(list(schedule_by_team.keys())[j]) 
-    schedule_by_match.update({list_of_teams[i]:matches})
+    match_number = line.pop(0)
 
-for i in range (0,len(schedule_by_match.keys())-1):
-    print(str(list(schedule_by_match.keys())[i])+": "+str(schedule_by_match.get(list(schedule_by_match.keys())[i])).replace('[','').replace(']','')) 
+    for index in line:
+        if index.isnumeric():
+            if index not in teams:
+                teams[index] = []
+            teams[index].append(match_number)
+
+for team in teams:
+    out = f'{team}: {teams[team].pop(0)}'
+
+    for match in teams[team]:
+        out += f', {match}'
+
+    print(out)
